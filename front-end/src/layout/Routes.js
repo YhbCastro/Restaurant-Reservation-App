a002097cec0b4b3f73f9b320aa569c92ad968803
Yasmine Tanase <85961954+YhbCastro@users.noreply.github.com>
2022-04-29 19:46:13 -0700

    Add files via upload

---
+++ front-end/src/layout/Routes.js
@@ -0,0 +1,57 @@
+import React from "react";
+import useQuery from "../utils/useQuery";
+import CreateReservation from '../reservations/CreateReservation'
+import EditReservation from '../reservations/EditReservation'
+import { Redirect, Route, Switch } from "react-router-dom";
+import Dashboard from "../dashboard/Dashboard";
+import NotFound from "./NotFound";
+import { today } from "../utils/date-time";
+import CreateTable from "../tables/CreateTable";
+import Search from "../search/Search";
+import Seat from "../reservations/Seat";
+
+
+/**
+ * Defines all the routes for the application.
+ *
+ * You will need to make changes to this file.
+ *
+ * @returns {JSX.Element}
+ */
+function Routes() {
+  const query = useQuery();
+  const date = query.get("date");  
+  return (
+    <Switch>
+      <Route exact={true} path="/">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact={true} path="/reservations">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard date={date ? date : today()} />
+      </Route>
+      <Route path="/reservations/:reservationId/edit">
+        <EditReservation />
+      </Route>
+      <Route path="/reservations/:reservationId/seat">
+        <Seat />
+      </Route>
+      <Route path="/reservations/new">
+        <CreateReservation />
+      </Route>
+      <Route path="/tables/new">
+        <CreateTable />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+}
+
+export default Routes;
